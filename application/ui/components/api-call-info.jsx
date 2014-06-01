7ded6bd829cc987670495142fa701acaedab4e74
Bob Eagan <bob@synapsestudios.com>
2014-06-01 10:17:40 -0700

    replace util.inspect with JSON.stringify and wrap code tags in pre tags for line breaks

--- application/ui/components/api-call-info.jsx
+++ application/ui/components/api-call-info.jsx
@@ -2,7 +2,6 @@
 'use strict';
 
 var React = require('react');
-var util  = require('util');
 
 module.exports = React.createClass({
     render : function()
@@ -12,9 +11,9 @@ module.exports = React.createClass({
                 <h3 className="data__header">Request URI</h3>
                 <code className="data__code">{this.props.request.uri}</code>
                 <h3 className="data__header">Request Headers</h3>
-                <code className="data__code">{util.inspect(this.props.request.headers)}</code>
+                <pre><code className="data__code">{JSON.stringify(this.props.request.headers, null, 4)}</code></pre>
                 <h3 className="data__header">Request Body</h3>
-                <code className="data__code">{util.inspect(this.props.request.body)}</code>
+                <pre><code className="data__code">{JSON.stringify(this.props.request.body, null, 4)}</code></pre>
             </div>
         );
 
@@ -26,9 +25,9 @@ module.exports = React.createClass({
                     <h3 className="data__header">Response Status</h3>
                     <code className="data__code">{this.props.response.status}</code>
                     <h3 className="data__header">Response Headers</h3>
-                    <code className="data__code data__code--failure">{util.inspect(this.props.response.headers)}</code>
+                    <pre><code className="data__code data__code--failure">{JSON.stringify(this.props.response.headers, null, 4)}</code></pre>
                     <h3 className="data__header">Response Body</h3>
-                    <code className="data__code data__code--success">{util.inspect(this.props.response.data)}</code>
+                    <pre><code className="data__code data__code--success">{JSON.stringify(this.props.response.data, null, 4)}</code></pre>
                 </div>
             );
         }

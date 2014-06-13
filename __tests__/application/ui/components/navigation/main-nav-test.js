92578b8a1a7c3a708bb7288e2217135238658218
Zachary Chavez <zach@synapsestudios.com>
2014-06-13 08:12:11 -0700

    Test that links are created for sections and resources.

--- __tests__/application/ui/components/navigation/main-nav-test.js
+++ __tests__/application/ui/components/navigation/main-nav-test.js
@@ -1,29 +1,51 @@
 /** @jsx React.DOM */
 /* jshint ignore:start */
 var testedFilePath = '../../../../../application/ui/components/navigation/main-nav.jsx';
+
 jest.dontMock(testedFilePath);
+jest.dontMock('underscore');
 
 describe('MainNav', function() {
-    it('contains links to the pages', function() {
+    it('contains links for sections and resources', function() {
         var MainNav = require(testedFilePath);
         var TestUtils = require('react/lib/ReactTestUtils');
 
         var config = {
             'resources' : {
-                'aResource' : [
-                    ''
-                ],
+                'Section' : [
+                    {
+                        name : 'Resource 1'
+                    },
+                    {
+                        name : 'Resource 2'
+                    }
+                ]
             }
         };
 
-        // Render the main nav in the document
         var mainNav = <MainNav config={config} />;
+        TestUtils.renderIntoDocument(mainNav);
+
+        links = TestUtils.scryRenderedDOMComponentsWithClass(mainNav, 'main-nav__link');
+
+        expect(links.length).toEqual(3);
+        expect(links[0].getDOMNode().textContent).toEqual('Section');
+        expect(links[1].getDOMNode().textContent).toEqual('Resource 1');
+        expect(links[2].getDOMNode().textContent).toEqual('Resource 2');
+    });
+
+    it('renders back link', function() {
+        var MainNav = require(testedFilePath);
+        var TestUtils = require('react/lib/ReactTestUtils');
+
+        var mainNav = <MainNav config={{}} />;
         TestUtils.renderIntoDocument(mainNav);
 
         var backLink = TestUtils.findRenderedDOMComponentWithClass(
             mainNav,
             'header__back-link'
         );
-        expect(backLink.getDOMNode().textContent).toEqual('Back to API List');
+
+        expect(backLink.getDOMNode().getAttribute('href')).toEqual('/');
     });
 });

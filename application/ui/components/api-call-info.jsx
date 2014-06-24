cc5206da8a76cde2467b98ad95f0f5d18bd9a506
Ben Youngblood <ben@synapsestudios.com>
2014-06-24 15:14:05 -0700

    refs #22 | format headers when necessary

--- application/ui/components/api-call-info.jsx
+++ application/ui/components/api-call-info.jsx
@@ -1,9 +1,17 @@
 /** @jsx React.DOM */
 'use strict';
 
+var _     = require('underscore');
 var React = require('react');
 
 module.exports = React.createClass({
+    formatHeadersFromObject : function(headersObject)
+    {
+        return _.map(headersObject, function(value, key) {
+            return key + ': ' + value;
+        }).join("\n");
+    },
+
     render : function()
     {
         var requestData = (
@@ -11,7 +19,7 @@ module.exports = React.createClass({
                 <h3 className="data__header">Request URI</h3>
                 <code className="data__code">{this.props.request.uri}</code>
                 <h3 className="data__header">Request Headers</h3>
-                <pre><code className="data__code">{JSON.stringify(this.props.request.headers, null, 4)}</code></pre>
+                <pre><code className="data__code">{this.formatHeadersFromObject(this.props.request.headers)}</code></pre>
                 <h3 className="data__header">Request Body</h3>
                 <pre><code className="data__code">{JSON.stringify(this.props.request.body, null, 4)}</code></pre>
             </div>
@@ -25,7 +33,7 @@ module.exports = React.createClass({
                     <h3 className="data__header">Response Status</h3>
                     <code className="data__code">{this.props.response.status}</code>
                     <h3 className="data__header">Response Headers</h3>
-                    <pre><code className="data__code data__code--failure">{JSON.stringify(this.props.response.headers, null, 4)}</code></pre>
+                    <pre><code className="data__code data__code--failure">{this.props.response.headers}</code></pre>
                     <h3 className="data__header">Response Body</h3>
                     <pre><code className="data__code data__code--success">{JSON.stringify(this.props.response.data, null, 4)}</code></pre>
                 </div>

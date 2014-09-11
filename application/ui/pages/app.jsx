b9f8af6618fc64ff5ae74cf01c6a2e83dd39eb7f
Chris <chris.shiplet@synapsestudios.com>
2014-09-11 15:48:38 -0700

    Refs #85 - Move header to root app component

--- application/ui/pages/app.jsx
+++ application/ui/pages/app.jsx
@@ -4,17 +4,45 @@
 var _          = require('underscore');
 var React      = require('react');
 var dispatcher = require('synapse-common/lib/dispatcher');
-
-var SiteHeader        = require('../layouts/header');
+var cx    = require('react/lib/cx');
+var Link  = require('react-router').Link;
 
 module.exports = React.createClass({
     displayName : 'App',
 
     render: function() {
-        var header = <SiteHeader slug='test-slug' name='test name' logo='test' />;
+        var backButton, apiLogo, linkParams, apiSummaryClasses;
+
+        linkParams = {apiSlug : this.props.slug};
+
+        apiSummaryClasses = cx({
+            'header__api-link'            : true,
+            'header__api-link--no-margin' : this.props.showBackButton
+        });
+
+        /*
+        if (this.props.showBackButton) {
+            backButton = <Link to='api-list' className='header__back-button'>&#xf104;</Link>;
+        }
+        */
+
+        if (this.props.logo) {
+            apiLogo = (
+                <img className='header__api-logo' src={this.props.logo} alt={this.props.name} />
+            );
+        }
+
         return (
             <div>
-                {header}
+                <header className='header'>
+                    {backButton}
+                    {/*<Link to='api-page' params={linkParams} className={apiSummaryClasses}>{apiLogo}{this.props.name}</Link>*/}
+                    <span className='header__branding'>
+                        <img src="../images/logos/livelydocs-logomark.png" alt="" />
+                        <span className='powered-by'>powered by</span>
+                        <span className='logo-type'>Lively</span>
+                    </span>
+                </header>
 
                 <this.props.activeRouteHandler/>
             </div>

9856fc264e83a5a029f4ce007b59542e95a56c32
Zachary Chavez <zach@synapsestudios.com>
2015-02-12 08:16:17 -0700

    Refs #108 - Don't discard endpoint values on request.

--- application/store/request.js
+++ application/store/request.js
@@ -42,7 +42,7 @@ module.exports = Fluxxor.createStore({
 
     onRequest : function(requestInfo)
     {
-        this.state.endpoint[requestInfo.endpointName] = this.getBlankEndpointDataObject();
+        this.state.endpoint[requestInfo.endpointName].response = null;
         this.state.requestInfo = requestInfo.requestInfo;
         this.state.endpointName = requestInfo.endpointName;
 

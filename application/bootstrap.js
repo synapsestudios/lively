fc2f867064e4eeea1a4130fe970d434bf5f192a6
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-02-24 09:59:12 -0700

    Refs #125 - Inject flux via context.

--- application/bootstrap.js
+++ application/bootstrap.js
@@ -5,11 +5,14 @@ var React    = require('react');
 window.React = React; // React Dev Tools requires this
 var routes   = require('./routes');
 var Router   = require('react-router');
+var flux     = require('./flux');
 
 require('./ui/scss/app');
 
 React.initializeTouchEvents(true);
 
 Router.run(routes, Router.HistoryLocation, function (Handler) {
-    React.render(React.createElement(Handler, null), window.document.body);
+    React.withContext({flux : flux}, function () {
+        React.render(React.createElement(Handler, null), window.document.body);
+    });
 });
--- application/routes.jsx
+++ application/routes.jsx
@@ -16,11 +16,9 @@ var ApiPage       = require('./ui/pages/api');
 var ApiSummary    = require('./ui/components/api-summary');
 var ApiResource   = require('./ui/components/api-resource');
 
-var flux          = require('./flux');
-
 module.exports = (
     <Route name={'app'} path={'/'} handler={App}>
-        <Route name={'api'} path={':apiSlug'} handler={ApiPage} flux={flux}>
+        <Route name={'api'} path={':apiSlug'} handler={ApiPage}>
             <Route name={'api-resource'} path={'*'} handler={ApiResource} />
             <DefaultRoute name={'api-summary'} handler={ApiSummary} />
         </Route>

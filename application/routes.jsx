639f90de8ead22ef91986be1bf580518407ae94c
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-02-24 16:15:53 -0700

    Refs #125 - Update routes to work with new version of router.

--- application/routes.jsx
+++ application/routes.jsx
@@ -4,12 +4,10 @@ var Router        = require('react-router');
 
 var React         = require('react');
 var Route         = Router.Route;
-var Routes        = Router.Routes;
 var DefaultRoute  = Router.DefaultRoute;
 var NotFoundRoute = Router.NotFoundRoute;
 
 var App           = require('./ui/pages/app');
-var NotFoundPage  = require('./ui/pages/404');
 var ApiListPage   = require('./ui/pages/api-list');
 var ApiPage       = require('./ui/pages/api');
 
@@ -17,12 +15,11 @@ var ApiSummary    = require('./ui/components/api-summary');
 var ApiResource   = require('./ui/components/api-resource');
 
 module.exports = (
-    <Route name={'app'} path={'/'} handler={App}>
-        <Route name={'api'} path={':apiSlug'} handler={ApiPage}>
-            <Route name={'api-resource'} path={'*'} handler={ApiResource} />
-            <DefaultRoute name={'api-summary'} handler={ApiSummary} />
+    <Route handler={App}>
+        <Route name='api-list' path='/'         handler={ApiListPage} />
+        <Route name='api'      path='/:apiSlug' handler={ApiPage}>
+            <Route        name='api-resource' handler={ApiResource} path='*' />
+            <DefaultRoute name='api-summary'  handler={ApiSummary} />
         </Route>
-        <DefaultRoute name={'api-list'} handler={ApiListPage} />
-        <NotFoundRoute name={'not-found'} handler={NotFoundPage} />
     </Route>
 );

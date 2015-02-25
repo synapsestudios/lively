1292f7d7a04d03fc83fc464bd602d008f3e9dc49
Zachary Chavez <zach@synapsestudios.com>
2015-02-25 16:45:01 -0700

    Refs #127 - Add config store.

--- application/stores.js
+++ application/stores.js
@@ -1,9 +1,11 @@
 'use strict';
 
+var ConfigStore  = require('./store/api-config');
 var Oauth2Store  = require('./store/oauth2');
 var RequestStore = require('./store/request');
 
 module.exports = {
+    ConfigStore  : new ConfigStore(),
     OAuthStore   : new Oauth2Store(),
     RequestStore : new RequestStore()
 };

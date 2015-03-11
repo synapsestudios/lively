89fa5b2efa84c2eb2a39c3345ac40143b04c4244
Adam Gerbens <adam@synapsestudios.com>
2015-03-11 16:42:05 -0700

    Refs #125 | Update webpack.js github login still gives error

--- webpack.js
+++ webpack.js
@@ -1,14 +1,36 @@
+var path             = require('path');
+var request          = require('request');
 var WebpackDevServer = require('webpack-dev-server');
 var webpack          = require('webpack');
 var config           = require('./webpack.config');
-var path             = require('path');
+var url              = require('url');
 
-new WebpackDevServer(webpack(config), {
-    contentBase        : path.resolve(__dirname + '/build'),
-    hot                : true,
-    noInfo             : true,
-    historyApiFallback : true
-}).listen(9001, 'localhost', function (err, result) {
+var server = new WebpackDevServer(webpack(config), {
+    contentBase : path.resolve(__dirname, 'build'),
+    hot         : true,
+    noInfo      : true
+});
+
+server.use(function (req, res, next) {
+    var ext = path.extname(req.url);
+
+    if ((ext === '' || ext === '.html') && req.url !== '/') {
+        req.pipe(request('http://' + req.hostname + ':9001')).pipe(res);
+    } else {
+        next();
+    }
+});
+
+server.use(function (req, res, next) {
+    var reqUrl = url.parse(req.url);
+    if (reqUrl.pathname === '/oauth2-redirect') {
+        require('./application/oauth2-proxy')(req, res);
+    } else {
+        next();
+    }
+});
+
+server.listen(9001, function (err, result) {
     if (err) {
         console.log(err);
     }
